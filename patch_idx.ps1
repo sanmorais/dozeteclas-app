11c541e379f6e393b4f422193a70eec300f6150b
Sandro Junior <sandroemjr@gmail.com>
2026-09-07 14:33:05 -0300

    Atualização da home, adição do destaque do criador de repertórios e playlists

---
+++ patch_idx.ps1
@@ -0,0 +1,1 @@
+﻿ = c:\Users\sandr\OneDrive\Documentos\Música\Doze teclas\App\Doze Teclas\index.html
--- patch_index.py
+++ patch_index.py
@@ -98,15 +98,6 @@
                 <p class="menu-desc">Cifras revisadas com transposição dinâmica de tom e sem simplificação.</p>
             </a>
 
-            <a href="repertorio.html" class="menu-item repertorio-destaque">
-                <span class="badge-feature">DESTAQUE</span>
-                <div class="icon-box">
-                    <svg viewBox="0 0 24 24"><path d="M19 3h-1V1h-2v2H8V1H6v2H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V8h14v11zM9 10H7v2h2v-2zm4 0h-2v2h2v-2zm4 0h-2v2h2v-2z"/></svg>
-                </div>
-                <span class="menu-label">Repertório Litúrgico</span>
-                <p class="menu-desc">Monte setlists para missas e encontros offline. Organize suas celebrações com transposição salva e impressão.</p>
-            </a>
-
             <a href="campo-harmonico-pro.html" class="menu-item">
                 <div class="icon-box">
                     <svg viewBox="0 0 24 24"><path d="M4 11h5V5H4v6zm0 7h5v-6H4v6zm6 0h5v-6h-5v6zm6 0h5v-6h-5v6zm-6-7h5V5h-5v6zm6-6v6h5V5h-5z"/></svg>
@@ -146,7 +137,6 @@
                 <span class="menu-label">Inversões de Acordes</span>
                 <p class="menu-desc">Domine a condução de vozes e mude a sonoridade da sua música.</p>
             </a>
-
         </nav>
     </section>
 
